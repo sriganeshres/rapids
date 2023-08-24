e6109015826d9e116d4bdc81509117250072abd0
sriganeshres <sriganeshthota12345@gmail.com>
2023-08-24 18:36:05 +0530

    Added Centrality folder
    Added only betweenness centality file

---
+++ Centrality_Impl/betweenness_cugraph.py
@@ -0,0 +1,12 @@
+import cugraph as cnx
+import networkx as nx
+import time 
+nodes = int(input())
+edges = int(input())
+t0 = time.time()
+G = nx.barabasi_albert_graph(10000, 500)
+t01 = time.time() - t0
+t1 = time.time()
+A = cnx.betweenness_centrality(G) # cugraph
+t2 = time.time() - t1 
+print(f"time taken to create the graph of {nodes}nodes and {edges} edges is {t01} milliseconds\n time to find betweenness_centrality is {t2} milliseconds" )
