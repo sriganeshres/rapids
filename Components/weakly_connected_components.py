9d6d72fde74745f8fa5a33dc8c4c815268a4e651
sriganeshres <sriganeshthota12345@gmail.com>
2023-09-20 03:27:50 +0530

    Added & Updated Implementations
    Updated: ECG ADDED: Components: Weakly Connected

---
+++ Components/weakly_connected_components.py
@@ -0,0 +1,21 @@
+import cugraph as cnx
+import networkx as nx
+import cudf
+import time
+import cupy as cp
+nodes = int(input())
+edges = int(input())
+t0 = time.time()
+G = nx.barabasi_albert_graph(nodes, edges)
+t01 = time.time() - t0
+t1 = time.time()
+components_cupy = cnx.weakly_connected_components(G)
+t2 = time.time()-t1
+components_df = cudf.DataFrame({'vertex': list(components_cupy.keys())})
+components_df['labels'] = components_df.index
+label_gpy = components_df.groupby('vertex')
+label_count = label_gpy.count()
+print('Total no of components ', len(label_count))
+largest_components = label_count.nlargest(n = 10, columns= 'labels')
+print(largest_components)
+print(t01, ' s ',  t2 , 's ')
